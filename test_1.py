8686c1d85225626435e66a7aa30e736a9b9305e4
Marta <martaw@uw.edu>
2019-04-01 11:47:59 -0700

    changed the way the path was determined so it uses the current working directory and then assumes there's a folder there called results that it can put stuff in

--- test_1.py
+++ test_1.py
@@ -5,9 +5,13 @@ Created on Mon Apr  1 11:18:23 2019
 @author: Marta
 """
 
-filename = '/results/test_1'
+import os
+
+cwd = os.getcwd()
+
+filename = str(cwd) + '/results/test_2.csv'
     
 file = open(filename, 'a')
-file.write('this is the first test')
+file.write('this is the second test')
 
 file.close()
