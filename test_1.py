200ffa6bb9ccd799c0f3b714cfa992306c3eba8a
Marta <martaw@uw.edu>
2019-04-01 11:22:18 -0700

    test file that writes a line of text to a csv file and saves it in results folder

---
+++ test_1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 11:18:23 2019
+
+@author: Marta
+"""
+
+filename = '/results/test_1'
+    
+file = open(filename, 'a')
+file.write('this is the first test')
+
+file.close()
